2df9270a29c253eb8d5a519b9566e5c843809f04
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-01 17:42:25 -0300

    Refactoring del Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Button.css'
+
 const Button = ({text, type, clickHandler}) =>
  (
         <button className ={type} onClick ={() =>{
