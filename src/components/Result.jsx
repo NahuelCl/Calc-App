97fd822547fa4ac6ed7866660d8cf0c16e3deea0
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-01 15:11:17 -0300

    Avances 2

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Result = ({value}) => {
     return (
@@ -6,6 +7,10 @@ const Result = ({value}) => {
             {value}
         </div>
     )
+}
+
+Result.propTypes = {
+    value : PropTypes.string.isRequired
 }
 
 export default Result
