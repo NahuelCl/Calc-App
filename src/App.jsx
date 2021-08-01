06397bb7eafa0cfa8f07fea489f7b09c5fc86b08
Nahuel Clauser <nahuelclauser@gmail.com>
2021-08-01 18:18:34 -0300

    Refactoring de los Numeros

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 //importancion
 import React from 'react'
-import Button from './components/Button'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
 import Result from './components/Result'
 
 import './App.css'
@@ -12,9 +12,6 @@ import './App.css'
 const App = () => {
     //La ejecucion
 
-    const clickHandlerFunction = (text) =>{
-        console.log("Button.clickHandler",text)
-    }
         
 
             // Lo que ejecuta la función
@@ -22,18 +19,10 @@ const App = () => {
     return (
     <main className='react-calculator'>
         <Result value={undefined} />
-        <div className="numbers">
-            <Button text="1" clickHandler = {clickHandlerFunction}/>
-            <Button text="2" clickHandler = {clickHandlerFunction} />
-            <Button text="3" clickHandler = {clickHandlerFunction}/>
-            <Button text="4" clickHandler = {clickHandlerFunction}/>
-            <Button text="5" clickHandler = {clickHandlerFunction}/>
-            <Button text="6" clickHandler = {clickHandlerFunction}/>
-            <Button text="7" clickHandler = {clickHandlerFunction}/>
-            <Button text="8" clickHandler = {clickHandlerFunction}/>
-            <Button text="9" clickHandler = {clickHandlerFunction}/>
-            <Button text="0" clickHandler = {clickHandlerFunction}/>
-        </div>
+
+        <Numbers onClickNumber={number => {
+            console.log("Click en number", number) 
+        }} />
         <Functions
             onContentClear = {clear =>
             console.log("Content Clear", clear)}
